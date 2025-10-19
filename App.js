3dabaf45e4c5458e61bd9a4b449c2300ace068e3
Talha12667 <sp23-bse-028@cuiatk.edu.pk>
2025-10-19 08:37:57 -0700

    Add TeacherMessage

--- App.js
+++ App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { View, ScrollView } from 'react-native';
 import { styles } from './src/styles/styles';
 import AboutMe from './src/components/custom/AboutMe';
+import TeacherMessage from './src/components/custom/AboutMe';
 import TasbihList from './src/components/custom/TasbihList';
-// import TeacherMessage from './src/components/custom/TeacherMessage'; // Students create this
 import SearchAndAdd from './src/components/custom/SearchAndAdd';
 import { STUDENT_NAME, REG_NO } from './src/config/student';
 
 export default function App() {
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <AboutMe name={STUDENT_NAME} regNo={REG_NO} />
-      {/* TODO: after creating the custom/TeacherMessage component, render it here */}
+      <AboutMe name={MuhammadTalhaRafique} regNo={SP23-BSE-028} />
+      <TeacherMessage />  {/*  Added below AboutMe */}
       <TasbihList />
       <SearchAndAdd />
     </ScrollView>
